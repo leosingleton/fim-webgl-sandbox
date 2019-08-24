734ab8de59b6b2c8824739820a30465168925fb3
Leo Singleton <leo@leosingleton.com>
2019-08-24 10:51:19 -0400

    Updating project name to WebGL Sandbox

--- src/Editor.ts
+++ src/Editor.ts
@@ -1,4 +1,4 @@
-// FIM - Fast Image Manipulation Library for Javascript
+// WebGL Sandbox
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
