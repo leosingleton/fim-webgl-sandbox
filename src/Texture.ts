0203dafcaab45cd6154887028e577110d4aea896
Leo Singleton <leo@leosingleton.com>
2019-09-18 07:24:30 -0400

    Consuming latest NPM packages

--- src/Texture.ts
+++ src/Texture.ts
@@ -2,6 +2,7 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
+import { fim } from './Common';
 import { FimCanvas } from '@leosingleton/fim';
 
 export class Texture {
@@ -17,7 +18,7 @@ export class Texture {
   public isRenaming = false;
 
   public static async createFromFile(file: File): Promise<Texture> {
-    let canvas = await FimCanvas.createFromImageBlob(file);
+    let canvas = await fim.createFromImageBlobAsync(file);
     return new Texture(file.name, canvas);
   }
 
